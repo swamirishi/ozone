c2c41a5c50f90d42a2740cf5dc1c803ff7c38ee6
Swaminathan Balachandran <swamirishi.sb@gmail.com>
2023-05-10 13:26:53 -0700

    HDDS-8477: Fix Testcase with null character

--- hadoop-hdds/rocksdb-checkpoint-differ/src/test/java/org/apache/ozone/rocksdb/util/TestManagedSstFileReader.java
+++ hadoop-hdds/rocksdb-checkpoint-differ/src/test/java/org/apache/ozone/rocksdb/util/TestManagedSstFileReader.java
@@ -119,7 +119,6 @@ public class TestManagedSstFileReader {
     }
   }
 
-
   @ParameterizedTest
   @ValueSource(ints = {0, 1, 2, 3, 7, 10})
   public void testGetKeyStreamWithTombstone(int numberOfFiles)
