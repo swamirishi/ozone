44919208afa100df66a8283986a99fcc8d8817a2
Siddhant Sangwan <siddhantsangwan027@gmail.com>
2023-12-20 11:54:06 +0530

    CDPD-63118. HDDS-9592. Replication Manager: Save UNHEALTHY replicas with highest BCSID for a QUASI_CLOSED container (#5794)
    (cherry picked from commit faa19906f664f3a68ccd3b1b9d6347dced279605)
    Change-Id: Id7e86699c64a48eaa0005018cfaffb348243aeef

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/container/replication/TestReplicationManagerUtil.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/container/replication/TestReplicationManagerUtil.java
@@ -17,11 +17,13 @@
  */
 package org.apache.hadoop.hdds.scm.container.replication;
 
+import org.apache.hadoop.hdds.client.RatisReplicationConfig;
 import org.apache.hadoop.hdds.protocol.DatanodeDetails;
 import org.apache.hadoop.hdds.protocol.MockDatanodeDetails;
 import org.apache.hadoop.hdds.protocol.proto.HddsProtos;
 import org.apache.hadoop.hdds.protocol.proto.StorageContainerDatanodeProtocolProtos.ContainerReplicaProto;
 import org.apache.hadoop.hdds.scm.container.ContainerID;
+import org.apache.hadoop.hdds.scm.container.ContainerInfo;
 import org.apache.hadoop.hdds.scm.container.ContainerReplica;
 import org.apache.hadoop.hdds.scm.node.NodeStatus;
 import org.apache.hadoop.hdds.scm.node.states.NodeNotFoundException;
@@ -38,6 +40,7 @@ import java.util.Set;
 import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.NodeOperationalState.DECOMMISSIONING;
 import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.NodeOperationalState.IN_MAINTENANCE;
 import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.NodeOperationalState.IN_SERVICE;
+import static org.apache.hadoop.hdds.scm.container.replication.ReplicationTestUtil.createContainer;
 import static org.apache.hadoop.hdds.scm.container.replication.ReplicationTestUtil.createContainerReplica;
 
 /**
@@ -54,7 +57,9 @@ public class TestReplicationManagerUtil {
 
   @Test
   public void testGetExcludedAndUsedNodes() throws NodeNotFoundException {
-    ContainerID cid = ContainerID.valueOf(1L);
+    ContainerInfo container = createContainer(HddsProtos.LifeCycleState.CLOSED,
+        RatisReplicationConfig.getInstance(HddsProtos.ReplicationFactor.THREE));
+    ContainerID cid = container.containerID();
     Set<ContainerReplica> replicas = new HashSet<>();
     ContainerReplica good = createContainerReplica(cid, 0,
         IN_SERVICE, ContainerReplicaProto.State.CLOSED, 1);
@@ -107,7 +112,7 @@ public class TestReplicationManagerUtil {
         });
 
     ReplicationManagerUtil.ExcludedAndUsedNodes excludedAndUsedNodes =
-        ReplicationManagerUtil.getExcludedAndUsedNodes(
+        ReplicationManagerUtil.getExcludedAndUsedNodes(container,
             new ArrayList<>(replicas), toBeRemoved, pending,
             replicationManager);
 
@@ -122,6 +127,91 @@ public class TestReplicationManagerUtil {
     Assertions.assertEquals(4, excludedAndUsedNodes.getExcludedNodes().size());
     Assertions.assertTrue(excludedAndUsedNodes.getExcludedNodes()
         .contains(unhealthy.getDatanodeDetails()));
+    Assertions.assertTrue(excludedAndUsedNodes.getExcludedNodes()
+        .contains(decommissioning.getDatanodeDetails()));
+    Assertions.assertTrue(excludedAndUsedNodes.getExcludedNodes()
+        .contains(remove.getDatanodeDetails()));
+    Assertions.assertTrue(excludedAndUsedNodes.getExcludedNodes()
+        .contains(pendingDelete));
+  }
+
+  @Test
+  public void testGetUsedAndExcludedNodesForQuasiClosedContainer() throws NodeNotFoundException {
+    ContainerInfo container = createContainer(HddsProtos.LifeCycleState.QUASI_CLOSED,
+        RatisReplicationConfig.getInstance(HddsProtos.ReplicationFactor.THREE));
+    ContainerID cid = container.containerID();
+    Set<ContainerReplica> replicas = new HashSet<>();
+    ContainerReplica good = createContainerReplica(cid, 0, IN_SERVICE,
+        ContainerReplicaProto.State.QUASI_CLOSED, 1);
+    replicas.add(good);
+
+    ContainerReplica remove = createContainerReplica(cid, 0,
+        IN_SERVICE, ContainerReplicaProto.State.QUASI_CLOSED, 1);
+    replicas.add(remove);
+    Set<ContainerReplica> toBeRemoved = new HashSet<>();
+    toBeRemoved.add(remove);
+
+    // this replica should be on the used nodes list
+    ContainerReplica unhealthyWithUniqueOrigin = createContainerReplica(
+        cid, 0, IN_SERVICE, ContainerReplicaProto.State.UNHEALTHY, 1);
+    replicas.add(unhealthyWithUniqueOrigin);
+
+    // this one should be on the excluded nodes list
+    ContainerReplica unhealthyWithNonUniqueOrigin = createContainerReplica(cid, 0, IN_SERVICE,
+        ContainerReplicaProto.State.UNHEALTHY, container.getNumberOfKeys(), container.getUsedBytes(),
+        MockDatanodeDetails.randomDatanodeDetails(), good.getOriginDatanodeId());
+    replicas.add(unhealthyWithNonUniqueOrigin);
+
+    ContainerReplica decommissioning =
+        createContainerReplica(cid, 0,
+            DECOMMISSIONING, ContainerReplicaProto.State.QUASI_CLOSED, 1);
+    replicas.add(decommissioning);
+
+    ContainerReplica maintenance =
+        createContainerReplica(cid, 0,
+            IN_MAINTENANCE, ContainerReplicaProto.State.QUASI_CLOSED, 1);
+    replicas.add(maintenance);
+
+    // Finally, add a pending add and delete. The add should go onto the used
+    // list and the delete added to the excluded nodes.
+    DatanodeDetails pendingAdd = MockDatanodeDetails.randomDatanodeDetails();
+    DatanodeDetails pendingDelete = MockDatanodeDetails.randomDatanodeDetails();
+    List<ContainerReplicaOp> pending = new ArrayList<>();
+    pending.add(ContainerReplicaOp.create(
+        ContainerReplicaOp.PendingOpType.ADD, pendingAdd, 0));
+    pending.add(ContainerReplicaOp.create(
+        ContainerReplicaOp.PendingOpType.DELETE, pendingDelete, 0));
+
+    Mockito.when(replicationManager.getNodeStatus(Mockito.any())).thenAnswer(
+        invocation -> {
+          final DatanodeDetails dn = invocation.getArgument(0);
+          for (ContainerReplica r : replicas) {
+            if (r.getDatanodeDetails().equals(dn)) {
+              return new NodeStatus(
+                  r.getDatanodeDetails().getPersistedOpState(),
+                  HddsProtos.NodeState.HEALTHY);
+            }
+          }
+          throw new NodeNotFoundException(dn.getUuidString());
+        });
+
+    ReplicationManagerUtil.ExcludedAndUsedNodes excludedAndUsedNodes =
+        ReplicationManagerUtil.getExcludedAndUsedNodes(container,
+            new ArrayList<>(replicas), toBeRemoved, pending,
+            replicationManager);
+
+    Assertions.assertEquals(4, excludedAndUsedNodes.getUsedNodes().size());
+    Assertions.assertTrue(excludedAndUsedNodes.getUsedNodes()
+        .contains(good.getDatanodeDetails()));
+    Assertions.assertTrue(excludedAndUsedNodes.getUsedNodes()
+        .contains(maintenance.getDatanodeDetails()));
+    Assertions.assertTrue(excludedAndUsedNodes.getUsedNodes()
+        .contains(pendingAdd));
+    Assertions.assertTrue(excludedAndUsedNodes.getUsedNodes().contains(unhealthyWithUniqueOrigin.getDatanodeDetails()));
+
+    Assertions.assertEquals(4, excludedAndUsedNodes.getExcludedNodes().size());
+    Assertions.assertTrue(excludedAndUsedNodes.getExcludedNodes()
+        .contains(unhealthyWithNonUniqueOrigin.getDatanodeDetails()));
     Assertions.assertTrue(excludedAndUsedNodes.getExcludedNodes()
         .contains(decommissioning.getDatanodeDetails()));
     Assertions.assertTrue(excludedAndUsedNodes.getExcludedNodes()
