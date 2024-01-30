4c1f329dad169146f0fc20372e21643c94e90c21
Nandakumar Vadivelu <nanda@apache.org>
2024-01-30 19:20:36 +0300

    CDPD-63165. HDDS-10231. ContainerStateManager should not finalize OPEN containers without a Pipeline. (#6123)
    (cherry picked from commit 04d6e0bc5421ac4c666122d6755a4d132fc39cf0)
    Change-Id: If6fa9aa4447c7664e0a4f9794244af4711fc2ca6

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/container/replication/TestReplicationManager.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/container/replication/TestReplicationManager.java
@@ -28,6 +28,7 @@ import org.apache.hadoop.hdds.protocol.MockDatanodeDetails;
 import org.apache.hadoop.hdds.protocol.proto.HddsProtos;
 import org.apache.hadoop.hdds.protocol.proto.StorageContainerDatanodeProtocolProtos.ContainerReplicaProto;
 import org.apache.hadoop.hdds.protocol.proto.StorageContainerDatanodeProtocolProtos.SCMCommandProto;
+import org.apache.hadoop.hdds.scm.HddsTestUtils;
 import org.apache.hadoop.hdds.scm.PlacementPolicy;
 import org.apache.hadoop.hdds.scm.container.ContainerID;
 import org.apache.hadoop.hdds.scm.container.ContainerInfo;
@@ -43,6 +44,9 @@ import org.apache.hadoop.hdds.scm.ha.SCMServiceManager;
 import org.apache.hadoop.hdds.scm.node.NodeManager;
 import org.apache.hadoop.hdds.scm.node.NodeStatus;
 import org.apache.hadoop.hdds.scm.node.states.NodeNotFoundException;
+import org.apache.hadoop.hdds.scm.pipeline.PipelineManager;
+import org.apache.hadoop.hdds.scm.server.StorageContainerManager;
+import org.apache.hadoop.hdds.security.token.ContainerTokenGenerator;
 import org.apache.hadoop.hdds.server.events.EventPublisher;
 import org.apache.hadoop.ozone.protocol.commands.DeleteContainerCommand;
 import org.apache.hadoop.ozone.protocol.commands.ReconstructECContainersCommand;
@@ -58,6 +62,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import java.io.IOException;
@@ -93,6 +98,7 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 
 /**
  * Tests for the ReplicationManager.
@@ -166,6 +172,16 @@ public class TestReplicationManager {
     // Ensure that RM will run when asked.
     Mockito.when(scmContext.isLeaderReady()).thenReturn(true);
     Mockito.when(scmContext.isInSafeMode()).thenReturn(false);
+
+    PipelineManager pipelineManager = mock(PipelineManager.class);
+    Mockito.when(pipelineManager.getPipeline(Matchers.any()))
+        .thenReturn(HddsTestUtils.getRandomPipeline());
+
+    StorageContainerManager scm = mock(StorageContainerManager.class);
+    Mockito.when(scm.getPipelineManager()).thenReturn(pipelineManager);
+    Mockito.when(scm.getContainerTokenGenerator()).thenReturn(ContainerTokenGenerator.DISABLED);
+
+    Mockito.when(scmContext.getScm()).thenReturn(scm);
   }
 
   private ReplicationManager createReplicationManager() throws IOException {
