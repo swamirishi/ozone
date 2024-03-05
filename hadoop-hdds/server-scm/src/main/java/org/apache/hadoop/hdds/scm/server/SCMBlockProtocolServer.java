59addb1f7aa5a2e45e494ccf7d96ee35b304024a
Doroszlai, Attila <6454655+adoroszlai@users.noreply.github.com>
2024-03-05 09:50:50 +0100

    CDPD-68597: HDDS-10430. Race condition around Pipeline#nodesInOrder (#6316)
    (cherry picked from commit ff1e4143ad22ae56a9476c180bb56cdb3556c487)
    Change-Id: I7d75ac6f98dcb035a628eacf361ed0789b9676d6

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/SCMBlockProtocolServer.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/SCMBlockProtocolServer.java
@@ -26,6 +26,7 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
 import org.apache.hadoop.fs.CommonConfigurationKeys;
