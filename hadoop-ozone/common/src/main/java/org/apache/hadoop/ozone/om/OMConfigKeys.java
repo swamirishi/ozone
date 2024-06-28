1fd32ec8a1645c1ad9981e302bff38ebae0a2b50
Hemant <iamgroot@cloudera.com>
2024-06-28 09:39:40 -0700

    CDPD-71792. Disabling SSTFilteringService as workaround for snapshot chain corruption issue (#43)
    CDPD-71792. Disabling SSTFilteringService as workaround for snapshot chain corruption issue
    
    Co-authored-by: Hemant Kumar <hemantadhvaniya@gmail.com>

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/OMConfigKeys.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/OMConfigKeys.java
@@ -376,7 +376,7 @@ public final class OMConfigKeys {
   public static final String OZONE_SNAPSHOT_SST_FILTERING_SERVICE_INTERVAL =
       "ozone.snapshot.filtering.service.interval";
   public static final String
-      OZONE_SNAPSHOT_SST_FILTERING_SERVICE_INTERVAL_DEFAULT = "60s";
+      OZONE_SNAPSHOT_SST_FILTERING_SERVICE_INTERVAL_DEFAULT = "-1";
 
   public static final String
       OZONE_SNAPSHOT_CHECKPOINT_DIR_CREATION_POLL_TIMEOUT =
