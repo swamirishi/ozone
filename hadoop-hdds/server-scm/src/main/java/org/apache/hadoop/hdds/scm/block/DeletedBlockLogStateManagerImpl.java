a33b6584dc912387ce957cd13a41a0167ef2d0db
Ashish Kumar <117710273+ashishkumar50@users.noreply.github.com>
2024-11-21 18:36:26 +0530

    CDPD-76221. HDDS-11713. Use seek to reach the start transaction. (#7460)
    (cherry picked from commit d6a54880fdb0a7d28d68e84ec5e667db1011ef92)

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/block/DeletedBlockLogStateManagerImpl.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/block/DeletedBlockLogStateManagerImpl.java
@@ -145,7 +145,9 @@ public class DeletedBlockLogStateManagerImpl
       @Override
       public TypedTable.KeyValue<Long, DeletedBlocksTransaction> seek(
           Long key) throws IOException {
-        throw new UnsupportedOperationException("seek");
+        iter.seek(key);
+        findNext();
+        return nextTx;
       }
 
       @Override
