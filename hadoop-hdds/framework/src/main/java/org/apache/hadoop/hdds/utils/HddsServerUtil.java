34f54477d48858c300cc1db1cfe5d2eb2e947c9a
SaketaChalamchala <saketa.chalamchala@gmail.com>
2025-03-20 04:52:46 -0700

    CDPD-80572. HDDS-12557. Add progress indicator for checkpoint tarball in leader OM (#8085)
    (cherry picked from commit a646b85b82c5158e15744ae849bb91c8072bbe66)

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/HddsServerUtil.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/utils/HddsServerUtil.java
@@ -618,18 +618,20 @@ public final class HddsServerUtil {
     }
   }
 
-  public static void includeFile(File file, String entryName,
+  public static long includeFile(File file, String entryName,
                                  ArchiveOutputStream archiveOutputStream)
       throws IOException {
     ArchiveEntry archiveEntry =
         archiveOutputStream.createArchiveEntry(file, entryName);
     archiveOutputStream.putArchiveEntry(archiveEntry);
+    long bytesWritten;
     try (FileInputStream fis = new FileInputStream(file)) {
-      IOUtils.copy(fis, archiveOutputStream);
+      bytesWritten = IOUtils.copy(fis, archiveOutputStream);
       archiveOutputStream.flush();
     } finally {
       archiveOutputStream.closeArchiveEntry();
     }
+    return bytesWritten;
   }
 
   // Mark tarball completed.
