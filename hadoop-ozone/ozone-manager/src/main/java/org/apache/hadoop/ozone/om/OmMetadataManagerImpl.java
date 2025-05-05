f9906ea2b022f2118b4b7a0858201260571270ef
Swaminathan Balachandran <47532440+swamirishi@users.noreply.github.com>
2025-05-05 13:59:59 -0400

    CDPD-80569. HDDS-12561. Reclaimable Rename entry filter for reclaiming renaming entries (#8054)
    Change-Id: I84c1be708f3589ad2d975292757093f00d81d030

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmMetadataManagerImpl.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/OmMetadataManagerImpl.java
@@ -2097,6 +2097,12 @@ public class OmMetadataManagerImpl implements OMMetadataManager,
     return renameKey.toString();
   }
   @Override
+  public String[] splitRenameKey(String renameKey) {
+    String[] splitVals = renameKey.split(OM_KEY_PREFIX);
+    return new String[]{splitVals[1], splitVals[2], splitVals[3]};
+  }
+
+  @Override
   public String getMultipartKey(long volumeId, long bucketId,
                                 long parentID, String fileName,
                                 String uploadId) {
